9d22da0ea1f52c973927e0b2aa4ed7deafc1b4ee
Balaji Varadharajan <balajivaradharajan@Mac.lan>
2025-05-08 15:04:17 -0400

    updated db connections

--- db_scripts/db_schema_creation.sql
+++ db_scripts/db_schema_creation.sql
@@ -1,21 +1,21 @@
 -- Run the following SQL command to create a separate database:
-CREATE DATABASE gatewaydb;
+CREATE DATABASE sapphire_gatewaydb;
 -- use postgres root username and password and connect to the gateway db and run the following commands
 
 -- Create a new user specifically for this database:
-CREATE USER gatewayadmin WITH PASSWORD 'gateway_pass';
+CREATE USER sapphire_gateway_admin WITH PASSWORD 'password';
 
 -- Grant the user permissions to the database:
-GRANT ALL PRIVILEGES ON DATABASE gatewaydb TO gatewayadmin;
+GRANT ALL PRIVILEGES ON DATABASE sapphire_gatewaydb TO sapphire_gateway_admin;
 
 -- Create a schema for organizing tables:
-CREATE SCHEMA gateway AUTHORIZATION gatewayadmin;
+GRANT ALL PRIVILEGES ON DATABASE sapphire_gatewaydb TO sapphire_gateway_admin;
 
 -- Set the default schema for the user:
-ALTER ROLE gatewayadmin SET search_path TO gateway;
+CREATE SCHEMA sapphire_gateway AUTHORIZATION sapphire_gateway_admin;
 
 -- Now, create the table inside the gateway schema:
-CREATE TABLE gateway.routes (
+CREATE TABLE sapphire_gateway.routes (
                                 id SERIAL PRIMARY KEY,
                                 route_id VARCHAR(255) UNIQUE NOT NULL,
                                 uri VARCHAR(255) NOT NULL,
@@ -27,7 +27,7 @@ CREATE TABLE gateway.routes (
 
 -- Insert Sample Data
 
-INSERT INTO gateway.routes (route_id, uri, predicates, filters, enabled) VALUES
+INSERT INTO sapphire_gateway.routes (route_id, uri, predicates, filters, enabled) VALUES
                                                                              ('member-management', 'lb:http://MEMBER-MANAGEMENT', '/api/v1/sapphire/mms/secured/member/**, /api/v1/sapphire/mms/secured/account/**', 'AuthenticationFilter', TRUE),
                                                                              ('member-management-public', 'lb:http://MEMBER-MANAGEMENT', '/api/v1/sapphire/mms/public/**', NULL, TRUE),
                                                                              ('premium-billing', 'lb:http://PREMIUM-BILLING', '/api/v1/sapphire/premium-billing/**', NULL, TRUE),
